85fbe3ae61ecbb5a25134389159a36f440643b23
hansel88 <hpn_x@hotmail.com>
2015-02-24 14:59:10 -0800

    k

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -9,12 +9,8 @@ public class BallScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-<<<<<<< HEAD
         rigidbody.AddForce(100f, 300f, 0);
-=======
-        rigidbody.AddForce(0f, 600f, 0);
->>>>>>> ffd8f70db949b7691ae2c5493b8f592122447cd4
-	
+
 	}
 	
 	// Update is called once per frame
