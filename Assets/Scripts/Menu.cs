9c5c3743efb6ce637bedba3105bcbe9c615c9857
reinole <reinhartsen.oc@gmail.com>
2015-03-02 12:10:13 +0100

    mainmenu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,6 +6,7 @@ public class Menu : MonoBehaviour {
 	// Use this for initialization
     void OnGUI()
     {
+        GUI.Button(
         if (GUI.Button(new Rect(Screen.width / 3.2f, Screen.height / 2.6f, Screen.width /2.8f, Screen.height / 4), ""))
         {
             Application.LoadLevel(1);
