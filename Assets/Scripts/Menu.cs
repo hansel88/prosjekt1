33553ab006b8cb06ba80c098bfbc9e47b66b67f9
reinole <reinhartsen.oc@gmail.com>
2015-03-03 12:46:49 +0100

    GMupdate

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 
-	// Use this for initialization
     void OnGUI()
     {
 
-        GUI.Button(new Rect(Screen.width / 3.2f, Screen.height / 2.6f, Screen.width / 2.8f, Screen.height / 4), "");
+        if(GUI.Button(new Rect(Screen.width / 3.2f, Screen.height / 2.6f, Screen.width / 2.8f, Screen.height / 4), ""));
         {
             Application.LoadLevel(1);
+        }
+
+        if(GUI.Button(new Rect(Screen.width / 3.2f, Screen.height / 1.6f, Screen.width / 2.8f, Screen.height / 4), ""));
+        {
+            Application.Quit();
         }
         
     }
