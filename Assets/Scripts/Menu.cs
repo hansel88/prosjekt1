4bc8eb47c670b474c8f2fd621857673e03379feb
hansel88 <hpn_x@hotmail.com>
2015-03-03 17:06:10 +0100

    lol

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 
+    public Button playButton;
+    public Button highScoreButton;
     void OnGUI()
     {
-
+        
+        /*
         if(GUI.Button(new Rect(Screen.width / 3.2f, Screen.height / 2.6f, Screen.width / 2.8f, Screen.height / 4), ""));
         {
             Application.LoadLevel("Scene1");
@@ -15,6 +19,7 @@ public class Menu : MonoBehaviour {
         {
             Application.Quit();
         }
-        
+        */
     }
+
 }
