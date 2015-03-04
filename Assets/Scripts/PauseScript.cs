33ff59fa6dc10726048f3eebb5e93bae2020708a
reinole <reinhartsen.oc@gmail.com>
2015-03-04 18:31:03 +0100

    TinyFixes

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -2,29 +2,6 @@
 using System.Collections;
 
 public class PauseScript : MonoBehaviour {
-/*
-    public Canvas pauseCanvas;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-	    if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            (gameObject.Find("Paddle").GetComponent("PaddleMovement") as MonoBehaviour).enabled = false;
-            (gameObject.Find("Main Camera").GetComponent("PaddleMovement") as MonoBehaviour).enabled = false;
-            pauseCanvas.enabled = true;
-            Time.timeScale = 0;
-            Screen.lockCursor = false;
-            Screen.showCursor = true;
-        }
-	}
- * 
- */
 
     Canvas canvas2;
 
